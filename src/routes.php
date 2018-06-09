127dc7df430f85a8b9fb666016e6c17f9f8068a0
arata <aratamagna@gmail.com>
2018-06-09 15:29:27 -0400

    order

--- src/routes.php
+++ src/routes.php
@@ -11,13 +11,6 @@ $app->get('/form', function (Request $request, Response $response, array $args) 
   return $response->withJson(json_encode(iterator_to_array($forms, false)), 200);
 });
 
-$app->get('/form/{id}', function (Request $request, Response $response, array $args) {
-  $formid = $args['id'];
-  $collection = $this->client->generadoc->form;
-  $form = $collection->findOne(['_id' => $formid]);
-  return $response->withJson(json_encode(iterator_to_array($form, false)), 200);
-});
-
 $app->post('/form', function ($request, $response, $args) {
   $input = $request->getParsedBody();
 
@@ -26,17 +19,17 @@ $app->post('/form', function ($request, $response, $args) {
   return $response->withJson($insertOneResult->getInsertedId(), 201);
 });
 
+$app->get('/form/{id}', function (Request $request, Response $response, array $args) {
+  $formid = $args['id'];
+  $collection = $this->client->generadoc->form;
+  $form = $collection->findOne(['_id' => $formid]);
+  return $response->withJson(json_encode(iterator_to_array($form, false)), 200);
+});
+
 $app->get('/form/input', function (Request $request, Response $response, array $args) {
   $collection = $this->client->generadoc->input;
   $inputlist = $collection->find();
   return $response->withJson(json_encode(iterator_to_array($inputlist, false)), 200);
-});
-
-$app->get('/form/input/{id}', function (Request $request, Response $response, array $args) {
-  $inputid = $args['id'];
-  $collection = $this->client->generadoc->input;
-  $input = $collection->findOne(['_id' => $inputid]);
-  return $response->withJson(json_encode(iterator_to_array($input, false)), 200);
 });
 
 $app->post('/form/input', function ($request, $response, $args) {
@@ -45,6 +38,13 @@ $app->post('/form/input', function ($request, $response, $args) {
   $collection = $this->client->generadoc->input;
   $insertOneResult = $collection->insertOne($input);
   return $response->withJson($insertOneResult->getInsertedId(), 201);
+});
+
+$app->get('/form/input/{id}', function (Request $request, Response $response, array $args) {
+  $inputid = $args['id'];
+  $collection = $this->client->generadoc->input;
+  $input = $collection->findOne(['_id' => $inputid]);
+  return $response->withJson(json_encode(iterator_to_array($input, false)), 200);
 });
 
 $app->post('/doc', function ($request, $response, $args) {
@@ -61,17 +61,17 @@ $app->get('/template', function (Request $request, Response $response, array $ar
   return $response->withJson(json_encode(iterator_to_array($templatelist, false)), 200);
 });
 
-$app->get('/template/{id}', function (Request $request, Response $response, array $args) {
-  $tempid = $args['id'];
-  $collection = $this->client->generadoc->template;
-  $template = $collection->findOne(['_id' => $tempid]);
-  return $response->withJson(json_encode(iterator_to_array($template, false)), 200);
-});
-
 $app->post('/template', function ($request, $response, $args) {
   $input = $request->getParsedBody();
 
   $collection = $this->client->generadoc->template;
   $insertOneResult = $collection->insertOne($input);
   return $response->withJson($insertOneResult->getInsertedId(), 201);
+});
+
+$app->get('/template/{id}', function (Request $request, Response $response, array $args) {
+  $tempid = $args['id'];
+  $collection = $this->client->generadoc->template;
+  $template = $collection->findOne(['_id' => $tempid]);
+  return $response->withJson(json_encode(iterator_to_array($template, false)), 200);
 });
